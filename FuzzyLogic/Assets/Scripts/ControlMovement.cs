98ee7499e43a5af333f1eab0690d0f21b9df5451
Sintija Krasiļņikova <2002959@uad.ac.uk>
2023-03-15 22:55:36 +0000

    Added project

---
+++ FuzzyLogic/Assets/Scripts/ControlMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+
+    private float hor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hor = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(hor * speed, 0, 0);
+    }
+}
