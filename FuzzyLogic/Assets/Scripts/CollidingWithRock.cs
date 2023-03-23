b4dbe3354f375d44fab4495a63b034bab217a176
Sintija Krasiļņikova <2002959@uad.ac.uk>
2023-03-23 16:48:59 +0000

    Added a collision counter

---
+++ FuzzyLogic/Assets/Scripts/CollidingWithRock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollidingWithRock : MonoBehaviour
+{
+    public Text collisionNumberText;
+    public int collisionNumber = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("car"))
+        {
+            collisionNumber++;
+        }
+    }
+
+    private void Update()
+    {
+        collisionNumberText.text = "Collisions with rocks: " + collisionNumber.ToString();
+    }
+}
